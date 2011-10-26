190239fdc5d2a33c869c12e89f1097a9b9b19bce
7rans <transfire@gmail.com>
2011-10-26 12:13:51 -0400

    Initial commit.

---
+++ .ruby
@@ -0,0 +1,38 @@
+---
+source:
+- meta
+authors:
+- name: T. Sawyer
+  email: transfire@gmail.com
+copyrights:
+- holder: Thomas Sawyer
+  year: '2011'
+  license: GPL-3
+replacements: []
+alternatives: []
+requirements:
+- name: detroit
+- name: yardstick
+dependencies: []
+conflicts: []
+repositories:
+- uri: git://github.com/rubyworks/detroit-yardstick.git
+  scm: git
+  name: upstream
+resources:
+  home: http://detroit.github.com/
+  code: http://github.com/detroit/detroit-yardstick
+  bugs: http://github.com/detroit/detroit-yardstick/issues
+  mail: http://groups.google.com/group/rubyworks-mailinglist
+extra: {}
+load_path:
+- lib
+revision: 0
+summary: Yardstick plugin for Detroit
+title: Detroit Yardstick
+version: 0.1.0
+name: detroit-yardstick
+description: ! "Yardstick plugin for Detroit build system. This plugin generates a
+  documentation \ncoverage report for YARD documentation during the post-document
+  phase."
+date: '2011-10-26'
---
+++ lib/detroit-yardstick.rb
@@ -0,0 +1,107 @@
+require 'detroit/tool'
+
+module Detroit
+
+  # Yardstick tool.
+  def Yardstick(options={})
+    Yardstick.new(options)
+  end
+
+  # Yardstick service.
+  #
+  class Yardstick < Tool
+
+    # List of paths to measure
+    #
+    # @param [Array<#to_s>, #to_s] path
+    #   optional list of paths to measure
+    #
+    # @return [undefined]
+    #
+    # @api public
+    attr_accessor :path
+
+    # The path to the file where the measurements will be written
+    #
+    # @param [String, Pathname] output
+    #   optional output path for measurements
+    #
+    # @return [undefined]
+    #
+    # @api public
+    attr_accessor :output
+
+    # Initialize attribute defaults.
+    #
+    # @return [undefined]
+    #
+    # @api public
+    def initialize_defaults
+      @path   = 'lib/**/*.rb'
+      @output = project.log + 'yardstick.txt'
+    end
+
+    # Measure the documentation and write to output.
+    #
+    # @example
+    #   tool.save  # (save measurement report to output)
+    #
+    # @return [undefined]
+    #
+    # @api public
+    def save
+      write_report { |io| ::Yardstick.measure(path).puts(io) }
+    end
+
+    # Measure the documentation and write to stdout.
+    #
+    # @example
+    #   tool.print  # (print measurement report to stdout)
+    #
+    # @return [undefined]
+    #
+    # @api public
+    def print
+      ::Yardstick.measure(path).puts($stdout)
+    end
+
+    # Detroit station for yardstick measure is post-document.
+    #
+    # @api public
+    def station_post_document
+      save
+    end
+
+    # Path to to man-page document.
+    def self.man_page
+      File.dirname(__FILE__)+'/../man/detroit-yardstick.5'
+    end
+
+  private
+
+    # Open up a report for writing
+    #
+    # @yield [io]
+    #   yield to an object that responds to #puts
+    #
+    # @yieldparam [#puts] io
+    #   the object that responds to #puts
+    #
+    # @return [undefined]
+    #
+    # @api private
+    def write_report(&block)
+      file = Pathname(output)
+      file.dirname.mkpath
+      file.open('w', &block)
+    end
+
+    #
+    def initialize_require
+      #require 'pathname'
+      require 'yardstick'
+    end
+
+  end
+
+end
